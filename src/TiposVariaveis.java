41b3e7793d2cb4327a57dea07aa8e66fdafac809
Waldemar Silva <55422758+wpfsilva@users.noreply.github.com>
2023-08-24 19:53:12 -0300

    docs : addCode

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = 1;
+        short numeroCurto2 = (short) numeroNormal;
+        String meuNome = "Waldemar Silva";
+        int numero = 5;
+        final double VALOR_DE_PI = 3.14159265358979323846;
+        long valorLongo = 129391239192L;
+        byte b = 1;
+        char c = 'a';
+        boolean boleano = true;
+        float f = 232.4f;
+
     }
 }
